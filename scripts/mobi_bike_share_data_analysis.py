cb568bcdcfcbbe99f6ad283c7a874191815dc271
Vivian Jin <vivianjin95@gmail.com>
2021-06-15 23:37:18 -0700

    Fix up notebook files

--- scripts/mobi_bike_share_data_analysis.py
+++ scripts/mobi_bike_share_data_analysis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[33]:
+# In[2]:
 
 
 import pandas as pd
@@ -12,36 +12,45 @@ import json
 from shapely.geometry import Point, Polygon
 
 
-# In[34]:
+# In[3]:
 
 
+# load in .shp files 
 street_map = gpd.read_file('zoning-districts-and-labels.shp')
 parks_map = gpd.read_file('parks.shp')
 shoreline_map = gpd.read_file('shoreline-2002.shp')
 
+# test out a plot
 fig, ax = plt.subplots(figsize = (15,15))
 street_map.plot(ax=ax)
 
 
-# In[35]:
+# In[5]:
 
 
+# read json data into a pandas DataFrame
 df = pd.read_json('stations.json', orient='stations')
-df = pd.json_normalize(data['stations'])
+df = pd.json_normalize(df['stations'])
 df
 
 
-# In[105]:
+# In[7]:
 
 
 # all stations
 pd.set_option('display.max_rows', None)
-geo_df['station_id']
-
-#geo_df.count - 202
+df['station_id']
 
 
-# In[37]:
+# In[11]:
+
+
+# store total count in variable
+total_num_stations = df.count()
+total_num_stations
+
+
+# In[12]:
 
 
 # create coordinates from lat/lon
@@ -51,7 +60,7 @@ geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
 geo_df.head()
 
 
-# In[109]:
+# In[13]:
 
 
 # plot all stations on map
@@ -72,17 +81,16 @@ parks_map.plot(ax=ax, markersize=20, color='red', marker='o', label = 'Park')
 ax.legend()
 
 
-# In[144]:
+# In[18]:
 
 
 # thought I would need lat/lon for park locations to calculate distances
 parks_df = pd.DataFrame(parks_map)
 parks_df['lat'] = [point.y for point in parks_df['geometry']]
 parks_df['lon'] = [point.x for point in parks_df['geometry']]
-parks_df
 
 
-# In[170]:
+# In[20]:
 
 
 # stations that are within 200m of a park
@@ -98,16 +106,25 @@ for index, row in geo_df.iterrows():
 count
 
 
-# In[104]:
+# In[22]:
+
+
+# % of stations that are within 200m of a park
+count/total_num_stations * 100
+
+
+# In[24]:
 
 
 # plot stations on shoreline map
 fig, ax = plt.subplots(figsize = (15,15))
+crs={'init':'epsg:4326'}
+geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
 shoreline_map.plot(ax=ax)
 geo_df.plot(ax=ax, markersize=20, color='blue', marker='o', label = 'Station')
 
 
-# In[171]:
+# In[25]:
 
 
 # stations that are within 200m of the shoreline
@@ -122,23 +139,35 @@ for index, row in geo_df.iterrows():
 count
 
 
-# In[81]:
+# In[26]:
 
 
+# % of stations that are within 200m of the shoreline
+count/total_num_stations * 100
+
+
+# In[27]:
+
+
+# manually filter for top 5 departure stations (found by SQL querying)
+crs={'init':'epsg:4326'}
+geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
 top_5_departure = geo_df[geo_df["station_id"].isin(['0209', '0105', '0102', '0066', '0036'])]
 top_5_departure.head()
 
 
-# In[82]:
+# In[28]:
 
 
+# manually filter for top 5 return stations (found by SQL querying)
 top_5_return = geo_df[geo_df["station_id"].isin(['0209', '0028', '0102', '0066', '0083'])]
 top_5_return.head()
 
 
-# In[78]:
+# In[29]:
 
 
+# manually filter for bottom 5 return and departure stations (found by SQL querying)
 bottom_5_return = geo_df[geo_df["station_id"].isin(['0129', '0221', '0165', '0227', '0066'])]
 bottom_5_return.head()
 
@@ -146,9 +175,10 @@ bottom_5_departure = geo_df[geo_df["station_id"].isin(['0221', '0066', '0129', '
 bottom_5_departure.head()
 
 
-# In[116]:
+# In[30]:
 
 
+# plot all points (ended up with 6 for each category due to overlap between return and departure stations)
 fig, ax = plt.subplots(figsize = (15,15))
 street_map.plot(ax = ax)
 top_5_return.plot(ax=ax, markersize=20, color='black', marker='o', label = 'Most Popular')
